caf08ca0d1883e18d7a78eb96b621a25e56210cd
Subash Balaji S <subashbalaji.god@gmail.com>
2026-04-20 20:27:55 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 1
+        for num in nums:
+            if num == res:
+                res += 1
+            elif num > res:
+                break
+        return res
