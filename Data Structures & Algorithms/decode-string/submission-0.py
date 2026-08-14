cc6e66856459250c60564126202ade60567aaf44
Subash Balaji S <subashbalaji.god@gmail.com>
2026-08-14 19:48:01 +0530

    Add: decode-string - submission-0

---
+++ Data Structures & Algorithms/decode-string/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        curr = ""
+        num = 0
+
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+
+            elif ch == '[':
+                stack.append((curr, num))
+                curr = ""
+                num = 0
+
+            elif ch == ']':
+                prev, repeat = stack.pop()
+                curr = prev + curr * repeat
+
+            else:
+                curr += ch
+
+        return curr  
